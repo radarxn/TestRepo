c7f4c8583a2b3a552a06128290b860eda446ed78
Big_Head <radarxn@163.com>
2015-06-21 15:26:03 +0800

    Print "Hello World!"

---
+++ src/org/lins/java/Java.java
@@ -0,0 +1,12 @@
+package org.lins.java;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+	
+	
+
+}
